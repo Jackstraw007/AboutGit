edea78081f929d7ee8e5a387aa1d0a2a136b4522
Eniac <984359007@qq.com>
2017-12-12 15:20:49 +0800

    第一次提交文件夹

---
+++ test/datetrans.py
@@ -0,0 +1,9 @@
+import time
+dt = "Apr/1992/22"
+#转换成时间数组
+timeArray = time.strptime(dt, "%b/%Y/%d")
+#转换成新的时间格式(20160505-20:28:54)
+dt_new = time.strftime("%Y-%m-%d",timeArray)
+
+##############################
+#Asc.py
